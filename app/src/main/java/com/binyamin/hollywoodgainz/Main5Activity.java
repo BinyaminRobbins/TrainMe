4acec75d9128d1ef0bb1cef0318b71fd8e577ad0
Binyamin Robbins <binyamin.robbins@gmail.com>
2020-04-19 15:40:22 +0300

    created custom listView layout w/gifs

--- app/src/main/java/com/binyamin/hollywoodgainz/Main5Activity.java
+++ app/src/main/java/com/binyamin/hollywoodgainz/Main5Activity.java
@@ -2,15 +2,18 @@ package com.binyamin.hollywoodgainz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -18,91 +21,62 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 public class Main5Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ListView listview;
-    ArrayAdapter arrayAdapter;
     ArrayAdapter<CharSequence> spinnerAdapter;
-    ArrayList<String> currentArrayList = new ArrayList<>();
     Spinner spinner;
-    int spinnerPosition;
+    CustomAdapter customAdapter;
     int currentButton;
-
-    //Images:
-    ArrayList<String[][][]> WorkoutURLs = new ArrayList<>();
-    String[][] theRockWorkoutUrls = {};
-    String[][] prattWorkoutURLs = {ImageURLs.prattWarmup,ImageURLs.prattArms,ImageURLs.prattCircuit};
-    String[][] efronWorkoutURLs = {ImageURLs.efronArms,ImageURLs.efronLegs,ImageURLs.efronChest};
-
-    static ImageView imageView;
-
-    //Method to Display the amount of reps (contained in the 3D String Workout Arrays) as a Toast.
-    public void setRepsText(int position) {
-        String[][][] currentWorkout = new String[3][][];
-        switch (currentButton) {
-            case 0:
-                currentWorkout = Main4Activity.theRockWorkout;
-                break;
-            case 1:
-                currentWorkout = Main4Activity.prattWorkout;
-                break;
-            case 2:
-                currentWorkout = Main4Activity.efronWorkout;
-                break;
-            default:
-                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-        }
-        Toast.makeText(this, currentWorkout[spinnerPosition][position][1], Toast.LENGTH_LONG).show();
-    }
+    int[][] prattImages = {gifs.prattWarmups,gifs.prattArms, gifs.prattCircuit};
+    int[][] efronImages = {gifs.prattWarmups,gifs.prattArms, gifs.prattCircuit};
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        spinnerPosition = position;
-        currentArrayList.clear();
-        if (currentButton == 1) {
-            String[][] prattWorkout = new String[2][];
-            switch (spinnerPosition) {
-                case 0:
-                    prattWorkout = Main4Activity.prattWorkout[0];
-                    break;
-                case 1:
-                    prattWorkout = Main4Activity.prattWorkout[1];
-                    break;
-                case 2:
-                    prattWorkout = Main4Activity.prattWorkout[2];
-                    break;
-            }
-            for (int i = 0; i < prattWorkout.length; i++) {
-                currentArrayList.add(prattWorkout[i][0]);
-            }
-            arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, currentArrayList);
-            listview.setAdapter(arrayAdapter);
+            listview.setAdapter(customAdapter);
+            customAdapter.list.clear();
+            Log.i("Cleared", Integer.toString(customAdapter.list.size()));
+            if (currentButton == 0) {
 
-        } else if (currentButton == 2) {
-            String[][] efronWorkout = new String[3][];
-            switch (spinnerPosition) {
-                case 0:
-                    efronWorkout = Main4Activity.efronWorkout[0];
-                    break;
-                case 1:
-                    efronWorkout = Main4Activity.efronWorkout[1];
-                    break;
-                case 2:
-                    efronWorkout = Main4Activity.efronWorkout[2];
-                    break;
+            } else if (currentButton == 1) {
+                switch (position) {
+                    case 0:
+                        for (int i = 0; i < Main4Activity.prattWorkout[0].length; i++) {
+                            customAdapter.list.add(new singlerow(prattImages[0][i], Main4Activity.prattWorkout[0][i][0], Main4Activity.prattWorkout[0][i][1]));
+                        }
+                        break;
+                    case 1:
+                        for (int i = 0; i < Main4Activity.prattWorkout[1].length; i++) {
+                            customAdapter.list.add(new singlerow(prattImages[1][i], Main4Activity.prattWorkout[1][i][0], Main4Activity.prattWorkout[1][i][1]));
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < Main4Activity.prattWorkout[2].length; i++) {
+                            customAdapter.list.add(new singlerow(prattImages[2][i], Main4Activity.prattWorkout[2][i][0], Main4Activity.prattWorkout[2][i][1]));
+                        }
+                        break;
+                }
+            } else if (currentButton == 2) {
+                switch (position) {
+                    case 0:
+                        for (int i = 0; i < Main4Activity.efronWorkout[0].length; i++) {
+                            customAdapter.list.add(new singlerow(efronImages[0][i], Main4Activity.efronWorkout[0][i][0], Main4Activity.efronWorkout[0][i][1]));
+                        }
+                        break;
+                    case 1:
+                        for (int i = 0; i < Main4Activity.efronWorkout[1].length; i++) {
+                            customAdapter.list.add(new singlerow(efronImages[1][i], Main4Activity.efronWorkout[1][i][0], Main4Activity.efronWorkout[1][i][1]));
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < Main4Activity.efronWorkout[2].length; i++) {
+                            customAdapter.list.add(new singlerow(efronImages[2][i], Main4Activity.efronWorkout[2][i][0], Main4Activity.efronWorkout[2][i][1]));
+                        }
+                        break;
+                }
             }
-            for (int i = 0; i < efronWorkout.length; i++) {
-                currentArrayList.add(efronWorkout[i][0]);
-            }
-            arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, currentArrayList);
-            listview.setAdapter(arrayAdapter);
+            customAdapter.notifyDataSetChanged();
         }
-
-    }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
@@ -114,13 +88,11 @@ public class Main5Activity extends AppCompatActivity implements AdapterView.OnIt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
-        Toast.makeText(this, "Select A Workout for Rep/Set Info", Toast.LENGTH_SHORT).show();
-
-        if (currentArrayList.size() >= 0) {
-            currentArrayList.clear();
-        }
-
         TextView currentCeleb = findViewById(R.id.textViewCurrentCeleb);
+        listview = findViewById(R.id.listView);
+        customAdapter = new CustomAdapter(this);
+        listview.setAdapter(customAdapter);
+
         spinner = findViewById(R.id.spinner);
         Intent intent = getIntent();
         currentButton = intent.getIntExtra("currentButton", -1);
@@ -132,11 +104,10 @@ public class Main5Activity extends AppCompatActivity implements AdapterView.OnIt
             case 1:
                 currentCeleb.setText(Main4Activity.prattTitle.getText().toString());
                 currentCeleb.setTextSize(42);
-                String[][] prattWorkout = Main4Activity.prattWorkout[0];
-                for (int i = 0; i < prattWorkout.length; i++) {
-                    currentArrayList.add(prattWorkout[i][0]);
+                for(int i = 0; i < Main4Activity.prattWorkout.length; i ++){
+                    customAdapter.list.add(new singlerow(prattImages[0][i],Main4Activity.prattWorkout[0][i][0],Main4Activity.prattWorkout[0][i][1]));
                 }
-
+                customAdapter.notifyDataSetChanged();
                 //spinner set to the starting workout for efron listView
                 spinnerAdapter = ArrayAdapter.createFromResource(this,
                         R.array.chrisPratt, android.R.layout.simple_spinner_item);
@@ -147,47 +118,70 @@ public class Main5Activity extends AppCompatActivity implements AdapterView.OnIt
                 currentCeleb.setTextSize(45);
                 //listView gets data from default Efron i.e. efronWorkout(0)
                 String[][] efronWorkout = Main4Activity.efronWorkout[0];
-                for (int i = 0; i < efronWorkout.length; i++) {
-                    currentArrayList.add(efronWorkout[i][0]);
-                }
                 //spinner set to the starting workout for efron listView
+
                 spinnerAdapter = ArrayAdapter.createFromResource(this,
                         R.array.zacEfron, android.R.layout.simple_spinner_item);
                 break;
             default:
                 currentCeleb.setText("Error in (int)Current Button");
         }
+
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
         spinner.setOnItemSelectedListener(this);
-
-        listview = findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, currentArrayList);
-        listview.setAdapter(arrayAdapter);
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "That Didn't Work!!!!", Toast.LENGTH_SHORT).show();
-                setRepsText(position);
-                String[][] currentImageArray = {};
-                String imageUri = " ";
-                switch (currentButton) {
-                    case 0:
-                        imageUri = theRockWorkoutUrls[spinnerPosition][position];
-                        break;
-                    case 1:
-                        imageUri = prattWorkoutURLs[spinnerPosition][position];
-                        break;
-                    case 2:
-                        imageUri = efronWorkoutURLs[spinnerPosition][position];
-                        break;
-                }
-                imageView = findViewById(R.id.imageView);
-                    ImageView imageView = findViewById(R.id.imageView);
-                    Picasso.with(getApplicationContext()).load(imageUri).into(imageView);
-
-            }
-        });
     }
 }
+    class singlerow {
+        String title;
+        String subTitle;
+        int img;
 
+        singlerow(int img, String title, String subTitle) {
+            this.img = img;
+            this.title = title;
+            this.subTitle = subTitle;
+        }
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        static ArrayList<singlerow> list;
+        Context c;
+
+        CustomAdapter(Context context) {
+            list = new ArrayList<>();
+            c = context;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = inflater.inflate(R.layout.singlerow, parent, false);
+            ImageView imageView = row.findViewById(R.id.imageView);
+            TextView titleTxt = row.findViewById(R.id.titleTxt);
+            TextView subTitleTxt = row.findViewById(R.id.subTitleTxt);
+
+            singlerow tmp = list.get(position);
+
+            imageView.setImageResource(tmp.img);
+            titleTxt.setText(tmp.title);
+            subTitleTxt.setText(tmp.subTitle);
+
+            return row;
+        }
+    }
---
+++ app/src/main/java/com/binyamin/hollywoodgainz/gifs.java
@@ -0,0 +1,26 @@
+package com.binyamin.hollywoodgainz;
+
+public class gifs {
+    static int[] prattWarmups = {
+            R.drawable.pratt_running,
+            R.drawable.pullup,
+            R.drawable.pushup,
+            R.drawable.squat
+    };
+    static int[] prattArms = {
+            R.drawable.lat_pulldown,
+            R.drawable.dubbell_row,
+            R.drawable.barbell_curls,
+            R.drawable.pushup,
+            R.drawable.con_curls
+    };
+    static  int[] prattCircuit = {
+            R.drawable.pratt_running,
+            R.drawable.powerclean,
+            R.drawable.benchpress,
+            R.drawable.boxjump,
+            R.drawable.situps,
+            R.drawable.russiantwist,
+            R.drawable.mountain_climbers
+    };
+}
